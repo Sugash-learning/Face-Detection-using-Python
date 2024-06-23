3b595235d3e0923029191b0b59d09ea2bc8300f2
Sugash S <158431841+Sugash-learning@users.noreply.github.com>
2024-06-23 17:33:03 +0530

    Add files via upload

---
+++ Face Detection/face_detector.py
@@ -0,0 +1,20 @@
+import cv2
+a = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+b = cv2.VideoCapture(0)
+
+while True:
+    c_rec , d_img = b.read()
+    e = cv2.cvtColor(d_img, cv2.COLOR_BGR2GRAY)
+    f = a.detectMultiScale(e, 1.3,6)
+
+    for (x1,y1,w1,h1) in f:
+        cv2.rectangle(d_img, (x1,y1), (x1+w1, y1+h1), (0,255,0), 5)
+    cv2.imshow('img',d_img)
+    h = cv2.waitKey(40) & 0xff
+    if h == 40:
+        break
+
+b.release()
+cv2.destroyAllWindows()
+
